2707649980c8f885c7fad60afd16ef171a98e79a
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-05-04 06:57:23 +0200

    Zadanie 1

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 4.2);
+        System.out.println("Współrzędna x: " + point.getX());
+        System.out.println("Współrzędna y: " + point.getY());
+    }
+}
